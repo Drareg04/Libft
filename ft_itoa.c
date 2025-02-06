29c2db64ea29d177001f0ac028c971d5cf80be63
Drareg04 <gerard.lorza@gmail.com>
2025-02-06 17:02:20 +0100

    itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: glorza-p <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/30 15:32:48 by glorza-p          #+#    #+#             */
-/*   Updated: 2025/02/06 15:49:39 by glorza-p         ###   ########.fr       */
+/*   Updated: 2025/02/06 17:02:01 by glorza-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,7 +46,7 @@ char	*ft_itoa(int n)
 	i = length(&n, &sign);
 	num = malloc((i + sign + 1) * sizeof(char));
 	if (!num)
-		return (NULL);
+		return ('\0');
 	num[--i + sign] = '\0';
 	while (n > 0)
 	{
